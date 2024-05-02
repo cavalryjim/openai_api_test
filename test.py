e13c055918eeb751f1a1c3ab74b582908552fdf5
James Davis <james.davisphd@gmail.com>
2024-05-02 12:37:43 -0500

    added functions

--- test.py
+++ test.py
@@ -2,22 +2,34 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+def main():
+    load_dotenv()
+    
 
-question = input("What do you want to ask ChatGPT? ")
+    messages = build_message()
 
-messages = [
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": question}
-]
+    chat = get_chatgpt_response(messages)
 
-# Create a chat completion
-chat = client.chat.completions.create(model="gpt-3.5-turbo", messages=messages)
+    # print(chat)
+    print(chat.choices[0].message.content)
 
-# print(chat)
-print(chat.choices[0].message.content)
+def build_message():
+    question = input("What do you want to ask ChatGPT? ")
 
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": question}
+    ]
+
+    return messages
+
+def get_chatgpt_response(msgs):
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    # Create a chat completion
+    chat = client.chat.completions.create(model="gpt-3.5-turbo", messages=msgs)
+    return chat
+
+main()
 
 # messages = [ {"role": "system", "content":  
 #               "You are a intelligent assistant."} ] 
